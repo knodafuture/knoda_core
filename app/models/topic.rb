52721746332926dd9e7771e2cac7a15f3186caea
Adam England <adam.n.england@gmail.com>
2014-01-06 14:29:41 -0600

    Updating topic scopes to be accurate

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < ActiveRecord::Base
-  scope :find_active, -> { where('hidden is false').order('sort_order ASC')}
+  scope :find_active, -> { where('hidden is false') }
+  scope :sorted, -> { order('sort_order ASC') }
   
   include Authority::Abilities
   self.authorizer_name = 'TopicAuthorizer'
