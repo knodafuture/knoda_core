f3182a8e3cd4a84b9c4fa79df5f001add379c612
Adam England <adam.n.england@gmail.com>
2014-01-06 13:37:14 -0600

    Updating topics ordering

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ActiveRecord::Base
-  scope :find_active, -> { where('hidden is false')}
+  scope :find_active, -> { where('hidden is false').order('id ASC')}
   
   include Authority::Abilities
   self.authorizer_name = 'TopicAuthorizer'
