50e1986a28e20f70984ac2118739bae0c50739e1
Adam England <adam.n.england@gmail.com>
2014-01-06 11:44:56 -0600

    Adding more code to the shared engine

---
+++ app/authorizers/challenge_authorizer.rb
@@ -0,0 +1,9 @@
+class ChallengeAuthorizer < ApplicationAuthorizer
+  def self.readable_by?(user)
+    true
+  end
+  
+  def self.set_seenable_by?(user)
+    true
+  end
+end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,5 @@
 class Topic < ActiveRecord::Base
-  scope :find_active, -> { where('hidden is false').order('name') }
-  scope :find_active_by_pattern, ->(pattern) { where('hidden is false and name like ?', '%'+pattern+'%').order('name') }
-  
+  scope :find_active, -> { where('hidden is false')}
   
   include Authority::Abilities
   self.authorizer_name = 'TopicAuthorizer'
