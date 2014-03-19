576691fe1cc8acadd3e9dcc843221b8a81ed18dd
Adam England <adam.n.england@gmail.com>
2014-03-19 11:44:41 -0500

    Moving to new, more generic activity model

--- app/notifiers/invitation_activity_notifier.rb
+++ app/notifiers/invitation_activity_notifier.rb
@@ -1,14 +1,12 @@
 class InvitationActivityNotifier
 
   def self.deliver(invitation)  
-    puts 'make activity'
     a = Activity.new
     a.user_id = invitation.recipient_user_id
     a.activity_type = 'INVITATION'
     a.invitation_code = invitation.code
     a.invitation_group_name =  invitation.group.name
     a.invitation_sender = invitation.user.username
-    a.title = "#{invitation.user.username} has invited you to join the group \"#{invitation.group.name}\""
     a.created_at = DateTime.now
     a.seen = false
     a.save!
