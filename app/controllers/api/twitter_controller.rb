5710f90dac48ee5e10a4949ec6213fd7c91fb580
Adam England <adam.n.england@gmail.com>
2014-05-02 16:42:28 -0500

    Fixing a concern scoping problem

--- app/controllers/api/twitter_controller.rb
+++ app/controllers/api/twitter_controller.rb
@@ -1,6 +1,14 @@
 class Api::TwitterController < ActionController::Base
 	include AuthenticationConcern
 
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: {error: 'required parameter missing'}, status: 422
+  end
+ 
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: {error: 'item not found'}, status: 404
+  end
+
 	def create
 		p = tweet_params
 
