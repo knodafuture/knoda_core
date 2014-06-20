b05b0ae029bd41cb3e97caa221685ef56e11c908
Adam England <adam.n.england@gmail.com>
2014-06-20 14:56:59 -0500

    Fixing bug with duplicate facebook accounts

--- app/models/social_account.rb
+++ app/models/social_account.rb
@@ -1,7 +1,11 @@
 class SocialAccount < ActiveRecord::Base
 	include Authority::Abilities
 	validate do |account|
-		if SocialAccount.where(:provider_name => account.provider_name, :provider_id => account.provider_id).where('id != ?', account.id).first()
+		q = SocialAccount.where(:provider_name => account.provider_name, :provider_id => account.provider_id)
+		if account.id
+			q.where('id != ?', account.id)
+		end
+		if q.first()
 			account.errors.add(:user_facing, "This account is already in use by another Knoda user.")
 		end
 	end
