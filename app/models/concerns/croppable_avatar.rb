731eb6dba64003ddfb1e205d7675bc942c0ab3aa
Adam England <adam.n.england@gmail.com>
2014-07-15 08:46:06 -0500

    Update activity images after avatar changes

--- app/models/concerns/croppable_avatar.rb
+++ app/models/concerns/croppable_avatar.rb
@@ -3,6 +3,7 @@ module CroppableAvatar extend ActiveSupport::Concern
   included do
     has_attached_file :avatar, :styles => { :big => "344х344>", :small => "100x100>", :thumb => "40x40"}, :default_url => ""
     validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "application/octet-stream"]
+    after_post_process :update_activity_images
   end
 
   module ClassMethods
@@ -12,15 +13,15 @@ module CroppableAvatar extend ActiveSupport::Concern
     @geometry ||= {}
     avatar_path = (avatar.options[:storage] == :s3) ? avatar.url(style) : avatar.path(style)
     @geometry[style] ||= Paperclip::Geometry.from_file(avatar_path)
-  end    
+  end
 
   def avatar_from_path(path)
     self.avatar = File.open(path)
-  end     
+  end
 
   def avatar_from_url(url)
     self.avatar = URI.parse(process_uri(url))
-  end      
+  end
 
   def cropping?
     !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
@@ -29,7 +30,7 @@ module CroppableAvatar extend ActiveSupport::Concern
   def reprocess_avatar
     avatar.reprocess!
   end
-  
+
   def avatar_image
     if self.avatar.exists?
       {
@@ -40,7 +41,18 @@ module CroppableAvatar extend ActiveSupport::Concern
     else
       nil
     end
-  end   
+  end
+
+  def update_activity_images
+    if self.avatar_file_name_changed?
+      new_url = self.avatar.to_s
+      new_url.gsub!('/original/', '/small/')
+      old_url = new_url.gsub(self.avatar_file_name, self.avatar_file_name_was)
+      old_url = old_url [/[^\?]+/]
+      Activity.where("image_url like ?", "#{old_url}%").update_all(:image_url => new_url)
+    end
+  end
+
   private
 
   def process_uri(uri)
@@ -49,5 +61,5 @@ module CroppableAvatar extend ActiveSupport::Concern
     open(uri, :allow_redirections => :safe) do |r|
       r.base_uri.to_s
     end
-  end         
-end  
+  end
+end
