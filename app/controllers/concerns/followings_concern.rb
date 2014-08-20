fc0c31019906404a14fb249f1531a17ee8036ccf
Adam England <adam.n.england@gmail.com>
2014-08-20 11:52:31 -0500

    Allow batch posts of followings

--- app/controllers/concerns/followings_concern.rb
+++ app/controllers/concerns/followings_concern.rb
@@ -1,10 +1,20 @@
 module FollowingsConcern extend ActiveSupport::Concern
   def create
-    @following = current_user.followings.build(:leader_id => params[:leader_id])
-    if @following.save
-      render :json => @following, :status => 201
+    if params[:leader_id]
+      @following = current_user.followings.build(:leader_id => params[:leader_id])
+      if @following.save
+        render :json => @following, :status => 201
+      else
+        render :json => @following.errors, :status => 500
+      end
     else
-      render :json => @following.errors, :status => 500
+      @followings = []
+      params[:_json].each do | following_list_params |
+        following_list_params.permit!
+        following = current_user.followings.create!(following_list_params)
+        @followings << following
+      end
+      render :json => @followings, :status => 201, root: false
     end
   end
 
