edb9f0b4804d949552e24ad7e3ca7c9a2ccf21e4
Adam England <adam.n.england@gmail.com>
2014-09-02 10:59:50 -0500

    Change activity title for followings

--- app/notifiers/following_activity_notifier.rb
+++ app/notifiers/following_activity_notifier.rb
@@ -3,7 +3,7 @@ class FollowingActivityNotifier
   def self.deliver(following)
     a = Activity.find_or_initialize_by(user_id: following.leader_id, target_user_id: following.user.id, activity_type: 'FOLLOWING')
     a.image_url = following.user.avatar_image[:small]
-    a.title = "#{following.user.username} is now following you on Knoda."
+    a.title = following.user.username
     a.created_at = DateTime.now
     a.seen = false
     a.save!
