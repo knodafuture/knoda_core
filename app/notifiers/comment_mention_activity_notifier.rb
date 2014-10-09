6ae204e2d20de4e33d72ada256ae8f604771f65d
Adam England <adam.n.england@gmail.com>
2014-10-08 22:10:10 -0500

    Bug fix

--- app/notifiers/comment_mention_activity_notifier.rb
+++ app/notifiers/comment_mention_activity_notifier.rb
@@ -3,6 +3,7 @@ class CommentMentionActivityNotifier
   def self.deliver(comment, recipient)
     a = Activity.find_or_initialize_by(user_id: recipient.id, prediction_id: comment.prediction.id, activity_type: 'MENTION')
     a.title = "#{comment.user.username} mentioned you in their comment."
+    a.prediction_body = comment.prediction.body
     a.comment_body = comment.text
     if comment.user.avatar_image
       a.image_url = comment.user.avatar_image[:small]
