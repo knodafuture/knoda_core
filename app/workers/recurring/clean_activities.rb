daa354cfb624a074c180cb4f5453529c99f9bc2d
Adam England <adam.n.england@gmail.com>
2014-10-09 09:20:24 -0500

    Clean comment and prediction mention activities after 60 days

--- app/workers/recurring/clean_activities.rb
+++ app/workers/recurring/clean_activities.rb
@@ -3,7 +3,7 @@ class Recurring::CleanActivities
 
   def perform
     ActiveRecord::Base.connection_pool.with_connection do
-      Activity.where(:seen => true, :activity_type => ['WON','LOST','COMMENT']).where('created_at < ?', DateTime.now - 60.days).delete_all
+      Activity.where(:seen => true, :activity_type => ['WON','LOST','COMMENT','PREDICTION_MENTION','COMMENT_MENTION']).where('created_at < ?', DateTime.now - 60.days).delete_all
     end
   end
 end
