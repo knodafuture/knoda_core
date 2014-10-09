5ddefa6ad21e2e2535361f41b7e940df4610dca2
Adam England <adam.n.england@gmail.com>
2014-10-09 09:15:00 -0500

    Splitting prediction & comment mentions up

--- app/notifiers/prediction_mention_activity_notifier.rb
+++ app/notifiers/prediction_mention_activity_notifier.rb
@@ -1,7 +1,7 @@
 class PredictionMentionActivityNotifier
 
   def self.deliver(prediction, recipient)
-    a = Activity.find_or_initialize_by(user_id: recipient.id, prediction_id: prediction.id, activity_type: 'MENTION')
+    a = Activity.find_or_initialize_by(user_id: recipient.id, prediction_id: prediction.id, activity_type: 'PREDICTION_MENTION')
     a.title = "Holla! #{prediction.user.username} mentioned you in their prediction."
     a.prediction_body = prediction.body
     if prediction.user.avatar_image
