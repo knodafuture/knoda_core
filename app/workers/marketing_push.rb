6d3ece5d7061a840da8467e3d18d042e0faf082e
Jeff Cailteux <jcailteux@knoda.com>
2014-10-23 12:56:05 -0500

    marketing push progress

--- app/workers/marketing_push.rb
+++ app/workers/marketing_push.rb
@@ -3,17 +3,32 @@ class MarketingPush
 
   def perform(params)
     ActiveRecord::Base.connection_pool.with_connection do
-      puts "hello marketing push"
-      puts params['userinput']
+      puts params
       message = params['bodyinput']
-      title = params['titleinput']
 
       #if one user
+      if params['userinput'] != ""
+        @user=User.where(["lower(username) = :username", {:username => params['userinput'].downcase }]).first
+        if params['pushtype'] == "Android" or params['pushtype'] == "All"
+          gcm = GCM.new("AIzaSyDSuv3FpA4NtTXJivbsfh28vixLn55DrlI")
+          response = gcm.send_notification(@user.android_device_tokens.pluck(:token), {data: {alert: message, type: 'm'}});
+        elsif params['pushtype'] == "iOS" or params['pushtype'] == "All"
 
-      #if contest
+        end
+      elsif params['contestinput'] != ""
+        @contest= Contest.where(["id = :id",{:id => params['contestinput']}])
+        if params['pushtype'] == "Android" or params['pushtype'] == "All"
 
-      #if platform
+        elsif params['pushtype'] == "iOS" or params['pushtype'] == "All"
 
+        end
+      elsif params['pushtype']
+        if params['pushtype'] == "Android" or params['pushtype'] == "All"
+
+        elsif params['pushtype'] == "iOS" or params['pushtype'] == "All"
+
+        end
+      end
     end
   end
 end
